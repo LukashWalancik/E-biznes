56b3775463dcbb04708fdab909cd16ac3f4111ac
Łukasz Walancik <lukasz.walancik.education@gmail.com>
2025-05-28 13:56:06 +0200

    clean up some garbage code

--- BookStore/client/app/page.tsx
+++ BookStore/client/app/page.tsx
@@ -1,14 +1,12 @@
 import Link from 'next/link';
 
 export default function HomePage() {
-  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
-
   return (
     <main>
       <div className="card-container">
         <div className="card">
           <h3>Lista dostępnych książek</h3>
-          <p>Zobaczy książki dostępne w Książkarni.</p>
+          <p>Zobacz książki dostępne w Książkarni.</p>
           <Link href="/books">Dostępne Książki</Link>
         </div>
       </div>
